5688fdcb86c4345566ac2fa62c6d07ab02f4d56f
Akbarjon <akbarisroilovdragon@gmain.com>
2022-04-15 14:45:14 -0400

    main part

---
+++ main/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+# Create your views here.
+def main(request):
+    return render(request, 'welcomepage.html')
+
+def loginPage(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        if request.method == 'POST':
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+
+            user = authenticate(request, username=username, password=password)
+
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+            else:
+                messages.info(request, 'Username OR password is incorrect')
+
+        context = {}
+        return render(request, 'login.html', context)
+
+
+@login_required(login_url='login')
+def logoutUser(request):
+    logout(request)
+    return redirect('login')
+
+
+@login_required(login_url='login')
+def home(request):
+    return render(request, 'home.html')
