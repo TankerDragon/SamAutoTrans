d647e348aab7c01d2fad6f7bc5d45edc9b187fba
Akbarjon <akbarisroilovdragon@gmain.com>
2022-04-18 10:25:07 -0400

    update

--- budget/views.py
+++ budget/views.py
@@ -1,5 +1,17 @@
+import re
 from django.shortcuts import render
 
+class number:
+    value = 0
+    def get(self):
+        return self.value
+    
+    def increase(self):
+        self.value += 1
+
+n = number()
 # Create your views here.
 def main(request):
-    return render(request, 'main.html')
+    n.increase()
+    num = n.get()
+    return render(request, 'budget.html', {'num': num})
--- main/views.py
+++ main/views.py
@@ -18,7 +18,7 @@ def loginPage(request):
 
             if user is not None:
                 login(request, user)
-                return redirect('home')
+                return redirect('budget')
             else:
                 messages.info(request, 'Username OR password is incorrect')
 
