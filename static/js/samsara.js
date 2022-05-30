cfbd1a44c2ffbcb893d6b33015a2f118574135fd
Akbarjon <akbarisroilovdragon@gmain.com>
2022-05-30 10:12:44 -0400

    *

--- static/js/samsara.js
+++ static/js/samsara.js
@@ -117,4 +117,26 @@ function sortTable() {
   for (i = 0; i < rows.length; i++) {
     rows[i].children[0].innerText = i + 1;
   }
+  search();
+}
+function search() {
+  // Declare variables
+  var input, filter, table, tr, td1, td2, i, txtValue;
+  input = document.getElementById("search-input");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("tbody");
+  tr = table.getElementsByTagName("tr");
+
+  // Loop through all table rows, and hide those who don't match the search query
+  for (i = 0; i < tr.length; i++) {
+    td1 = tr[i].getElementsByTagName("td")[6];
+    if (td1) {
+      txtValue = td1.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }
+  }
 }
